9a35836ce5a8c0787855ee35fee6ae9d494b1e35
09ubberboy90 <2330834a@student.gla.ac.uk>
2022-02-14 16:38:51 +0000

    Added gui parameter

--- pybullet_panda/launch/stack_cubes.launch.py
+++ pybullet_panda/launch/stack_cubes.launch.py
@@ -6,6 +6,8 @@ from launch import LaunchDescription, LaunchDescriptionSource
 from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
 
 
 
@@ -13,12 +15,15 @@ def generate_launch_description():
     pkg_name = "pybullet_panda"
     pkg_share = get_package_share_directory(pkg_name)
 
+    gui = LaunchConfiguration("gui")
 
     panda = Node(name='panda',
                 package=pkg_name,
                 executable='panda',
                 output='screen',
+                parameters=[{"gui":gui}]
             )
+
 
     run_move_group_node = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -54,6 +59,11 @@ def generate_launch_description():
     ])))
 
     return LaunchDescription([
+        DeclareLaunchArgument(
+        'gui',
+        default_value="true",
+        description='GUI'),
+
         panda,
         TimerAction(
             period=5.,
--- sim_recorder/sim_recorder/run_recording.py
+++ sim_recorder/sim_recorder/run_recording.py
@@ -35,6 +35,7 @@ import subprocess
 import sys
 import time
 from multiprocessing import Event, Pipe, Process, Queue
+import argparse
 
 import psutil
 
@@ -48,14 +49,15 @@ except ModuleNotFoundError:
 import _thread
 import threading
 import re
-class PyBullet():
-    def __init__(self):
+class Pybullet():
+    def __init__(self, gui=False):
         self.name = "pybullet"
-        self.timeout = 600 # 10 minute
+        self.timeout = 900 # 15 minute
         self.commands = [
-            "ros2 launch pybullet_panda stack_cubes.launch.py",
+            f"ros2 launch pybullet_panda stack_cubes.launch.py gui:={str(gui).lower()}"
         ]
-        self.delays = [5] #added the timer delay from launch file + 10 s for robot movement
+        self.delays = [5] #added the timer delay from launch file
+
 def kill_proc_tree(pids, procs, interrupt_event, including_parent=False):
     interrupt_event.set()
     for pid in pids:
@@ -171,16 +173,18 @@ def run(sim, idx, path):
 
 def main(args=None):
     fail = 0
-    start_idx = 1
-    sim = PyBullet()
 
-    if len(sys.argv) == 2:
-        iteration = int(sys.argv[1])
-    elif len(sys.argv) == 3:
-        iteration = int(sys.argv[1])
-        start_idx = int(sys.argv[2])
-    else:
-        iteration = 1
+    parser = argparse.ArgumentParser(description='Sim recorder parameters')
+    parser.add_argument("-i", '--iterations', type=int, default=1,
+                        help='Number of iterations of the simulation')
+    parser.add_argument("-s", '--start-index', type=int, default=1,
+                        help='Allow to start the simulation at a different index then 1')
+    parser.add_argument('--headless', action='store_true',
+                        help='Whetever to render to a GUI or not')
+
+    args = parser.parse_args()
+    gui = True if not args.headless else False
+    sim = Pybullet(gui)
     dir_path = os.path.dirname(os.path.realpath(__file__))
     path = os.path.join(dir_path, "..")
     try:
@@ -211,9 +215,9 @@ def main(args=None):
     if os.path.exists(path+f"/{sim.name}/run.txt"):
         os.remove(path+f"/{sim.name}/run.txt")
 
-    for idx in range(start_idx, iteration+1):
+    for idx in range(args.start_index, args.iterations+1):
         fail += run(sim, idx, path)
-    print(f"Completed {iteration-fail}; Timeout {fail}")
+    print(f"Completed {args.iterations-fail}; Timeout {fail}")
 
 
 if __name__ == "__main__":
